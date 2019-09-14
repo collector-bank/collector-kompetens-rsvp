69050046b3c546e0558dff8f87116c559a4c83c7
Glitch (collector-bank-collector-kompetens-rsvp) <none>
2019-09-14 12:59:28 +0000

    📅🏛 Updated with Glitch2

--- utils/checkauth.js
+++ utils/checkauth.js
@@ -8,13 +8,7 @@ module.exports = {
       res.send(401);
     },
   
-    isAdmin: function(oid) {
-      return admins.length == 0 ? true : admins.includes(oid);
-    },
-
-  /*
-    ensureAuthenticatedAndAdmin: function (req, res, next) {
-      if (req.isAuthenticated() && this.isAdmin(req.user.oid)) { return next(); }
-      res.send(401);
-    }*/
+    isAdminUser: function(userEmail) {
+      return admins.length == 0 ? true : admins.map((x) => x.toLowerCase()).includes(userEmail.toLowerCase());
+    }
 }
